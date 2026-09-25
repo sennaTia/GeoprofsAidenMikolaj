a0bbe7bc2cdb1b1eb21e8c3a85c4f88f1c8a12c4
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-09-25 15:30:33 +0200

    .

--- geoprofs/geoprofs/app/Models/User.php
+++ geoprofs/geoprofs/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -45,7 +46,6 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-}
     public function leaveRequests(): HasMany
     {
         return $this->hasMany(LeaveRequest::class);
